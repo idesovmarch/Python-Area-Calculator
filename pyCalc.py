8503121ea781d481ddf41aa7a1af081812f95c87
Brad <30424247+idesovmarch@users.noreply.github.com>
2018-11-13 13:44:35 -0500

    Update pyCalc.py

--- pyCalc.py
+++ pyCalc.py
@@ -3,15 +3,13 @@ print "Calculator Booting"
 option = raw_input("Enter C for Circle or T for Triangle: ")
 
 if option == 'C':
-  radius = float(raw_input("Please enter the circle's radius"))
-  area = 3.14159 * (radius ** 2)
-  print "The circle's radius is %" (area)
+	radius = float(raw_input("Please enter the circles radius: "))
+	area = 3.14159 * (radius ** 2)
+	print "The circles radius is %s :" % area  
 elif option == 'T':
-  base = float(raw_input("Please enter the triangle's base"))
-  height = float(raw_input("Please enter the triangle's height"))
-  area = .5(b * h)
-  print "The triangle's area is %" (area)
-else print "Error: Invalid Shape"
-
-
-print "Calculator Exiting"
+	base = float(raw_input("Please enter the triangles base"))
+	height = float(raw_input("Please enter the triangles height: "))
+	area = 0.5 * b * h
+	print "The triangle's area is %s" % area
+else :
+	print "Error: Invalid Shape"
